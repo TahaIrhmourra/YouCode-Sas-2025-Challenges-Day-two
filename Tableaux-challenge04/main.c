6f5a99e3357f60114159a4a8a9b9d0abf72e3344
Taha Irhmourra <irhmourrataha@outlook.com>
2025-09-03 11:01:52 +0100

    Dans le quatrième challenge, j’ai appris à comparer les nombres dans un tableau et trouver le maximum.

---
+++ Tableaux-challenge04/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+    int nombreV, max, i;
+    printf("Veuillez le nombre des valuer dans le tableaux: ");
+    scanf("%d", &nombreV);
+    i = 0;
+    int tableaux[nombreV];
+    do {
+        printf("Veuillez enter la valeur %d: ", i + 1);
+        scanf("%d", &tableaux[i]);
+        i++;
+    } while (i < nombreV);
+    printf("\n-----------------\n");
+    max = 0;
+    for (i = 0; i < nombreV - 1; i++) {
+        if (tableaux[i] > tableaux[i + 1]) {
+            tableaux[i + 1] = tableaux[i];
+            max = tableaux[i + 1];
+        }
+    }
+    printf("La valeurs minimum dans le tableaux est: %d", max);
+    return 0;
+}
